8948155ff44d37db2a15a7922f7a19b7838a68d4
julname <mizar.zulmi1073@students.unila.ac.id>
2020-01-06 10:29:41 +0700

    nambah index

---
+++ iCoffee/resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>aa</title>
+</head>
+<body>
+<h2>NYOBA GUYS</h2>
+</body>
+</html>
--- iCoffee/resources/views/welcome.blade.php
+++ iCoffee/resources/views/welcome.blade.php
@@ -5,8 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
+                <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
