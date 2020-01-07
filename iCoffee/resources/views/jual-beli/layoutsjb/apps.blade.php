460f56106d2a3521b06d5c93936d63450b1e01cc
Fauzi Riski <intip.kirito@gmail.com>
2020-01-07 20:00:37 +0700

    update yang udh dibenerin jualbeli

--- iCoffee/resources/views/jual-beli/layoutsjb/apps.blade.php
+++ iCoffee/resources/views/jual-beli/layoutsjb/apps.blade.php
@@ -49,6 +49,10 @@
         <span class="text">Jl. Pagar Alam No.44, Kedaton</span>
       </div>
     </div>
+  </div>
+  </div>
+  </div>
+</div>
 
 
 	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
@@ -64,14 +68,6 @@
 	           <li class="nav-item">
 				 <a href="/jual-beli" class="nav-link">JualBeli</a>
 
-              <!-- <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
-              <div class="dropdown-menu" aria-labelledby="dropdown04">
-              	<a class="dropdown-item" href="shop.html">Shop</a>
-              	<a class="dropdown-item" href="wishlist.html">Wishlist</a>
-                <a class="dropdown-item" href="product-single.html">Single Product</a>
-                <a class="dropdown-item" href="cart.html">Cart</a>
-                <a class="dropdown-item" href="checkout.html">Checkout</a>
-              </div> -->
             </li>
 
 
@@ -96,7 +92,6 @@
   @yield('content')
 
 
-
   <footer class="ftco-footer ftco-section">
     <div class="container">
       <div class="row">
@@ -106,83 +101,79 @@
           </a>
         </div>
       </div>
+      <div class="row mb-5">
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4">
+      <h2 class="ftco-heading-2"><img src="{{asset('Jualbeli/logo.png') }}" width="35%" height="8%"></h2>
+      <h2 >Hubungi</h2>
+      <div class="block-23 mb-3">
+      <ul>
+        <li><span class="icon icon-map-marker"></span><span class="text">Jl. Pagar Alam (Gang PU) No.44 Kedaton Bandar Lampung 35145</span></li>
+        <li><a href="#"><span class="icon icon-phone"></span><span class="text">+62 811 7861 595</span></a></li>
+        <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+        </ul>
 
+      </div>
 
+            <!-- <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+              <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+              <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+              <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+            </ul> -->
           </div>
-          <div class="row mb-5">
-            <div class="col-md">
-              <div class="ftco-footer-widget mb-4">
-          <h2 class="ftco-heading-2"><img src="{{asset('Jualbeli/logo.png') }}" width="35%" height="8%"></h2>
-          <h2 >Hubungi</h2>
-          <div class="block-23 mb-3">
-          <ul>
-            <li><span class="icon icon-map-marker"></span><span class="text">Jl. Pagar Alam (Gang PU) No.44 Kedaton Bandar Lampung 35145</span></li>
-            <li><a href="#"><span class="icon icon-phone"></span><span class="text">+62 811 7861 595</span></a></li>
-            <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
+        </div>
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4 ml-md-5">
+            <h2 class="ftco-heading-2">Tentang iCoffee</h2>
+            <ul class="list-unstyled">
+              <li><a href="#" class="py-2 d-block">Tentang Kami</a></li>
+              <li><a href="#" class="py-2 d-block">FAQ</a></li>
+              <li><a href="#" class="py-2 d-block">Panduan Pengguna</a></li>
+      <li><a href="#" class="py-2 d-block">Blog</a></li>
+      <li><a href="#" class="py-2 d-block">Karir</a></li>
             </ul>
-  
           </div>
-  
-
-                <!-- <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                  <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                  <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                  <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-                </ul> -->
-              </div>
-            </div>
-            <div class="col-md">
-              <div class="ftco-footer-widget mb-4 ml-md-5">
-                <h2 class="ftco-heading-2">Tentang iCoffee</h2>
-                <ul class="list-unstyled">
-                  <li><a href="#" class="py-2 d-block">Tentang Kami</a></li>
-                  <li><a href="#" class="py-2 d-block">FAQ</a></li>
-                  <li><a href="#" class="py-2 d-block">Panduan Pengguna</a></li>
-                  <li><a href="#" class="py-2 d-block">Blog</a></li>
-                  <li><a href="#" class="py-2 d-block">Karir</a></li>
-                </ul>
-              </div>
-            </div>
-            <div class="col-md-4">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Pembeli</h2>
-              <div class="d-flex">
-                <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
-                  <li><a href="#" class="py-2 d-block">Cara Beli di iCoffee</a></li>
-                  <li><a href="#" class="py-2 d-block">Cara Pengembalian Barang (Pembeli)</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Penjual</h2>
-              <div class="d-flex">
-                <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
-                  <li><a href="#" class="py-2 d-block">Cara Jualan di iCoffee</a></li>
-                  <li><a href="#" class="py-2 d-block">Cara Pengembalian Barang 
-                  (Penjual)</a></li>
-                </ul>
-              </div>
+        </div>
+        <div class="col-md-4">
+           <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">Pembeli</h2>
+            <div class="d-flex">
+              <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
+                <li><a href="#" class="py-2 d-block">Cara Beli di iCoffee</a></li>
+        <li><a href="#" class="py-2 d-block">Cara Pengembalian Barang (Pembeli)</a></li>
+              </ul>
             </div>
           </div>
         </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
-            
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
+        <div class="col-md">
+          <div class="ftco-footer-widget mb-4">
+            <h2 class="ftco-heading-2">Penjual</h2>
+      <div class="d-flex">
+        <ul class="list-unstyled mr-l-5 pr-l-3 mr-4">
+          <li><a href="#" class="py-2 d-block">Cara Jualan di iCoffee</a></li>
+          <li><a href="#" class="py-2 d-block">Cara Pengembalian Barang 
+          (Penjual)</a></li>
+        </ul>
+        </div>
           </div>
         </div>
       </div>
-    </footer>
-    
-    
-    
-    <!-- loader -->
-    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+      <div class="row">
+        <div class="col-md-12 text-center">
+
+          <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+
+
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
 
 
 
