dfd79431a7893880bf81bad9204148af0eecaf4d
Fauzi Riski <intip.kirito@gmail.com>
2020-01-09 10:26:57 +0700

    update halaman login

--- iCoffee/routes/web.php
+++ iCoffee/routes/web.php
@@ -14,33 +14,33 @@
 // Route::group(['middleware'=>'redirect'],function (){
 // Route::group(['middleware'=>'admin'],function (){
 //admin
-	Route::get('/admin','AdminController@index');
-	Route::get('/super-admin','SuperAdminController@index');
+// 	Route::get('/admin','AdminController@index');
+// 	Route::get('/super-admin','SuperAdminController@index');
 
-//admin jual-beli
-	Route::get('/validasi-pembeli','AdminController@validasiPembeli');
-	Route::get('/jenis-produk','AdminController@jenisProduk');
-	Route::get('/laporan-penjualan','AdminController@laporanPenjualan');
+// //admin jual-beli
+// 	Route::get('/validasi-pembeli','AdminController@validasiPembeli');
+// 	Route::get('/jenis-produk','AdminController@jenisProduk');
+// 	Route::get('/laporan-penjualan','AdminController@laporanPenjualan');
 
-//admin Lelang
-	Route::get('/validasi-produk-lelang','AdminController@validasiProdukLelang');
-	Route::get('/proses-lelang','AdminController@prosesLelang');
-	Route::get('/laporan-lelang','AdminController@laporanLelang');
+// //admin Lelang
+// 	Route::get('/validasi-produk-lelang','AdminController@validasiProdukLelang');
+// 	Route::get('/proses-lelang','AdminController@prosesLelang');
+// 	Route::get('/laporan-lelang','AdminController@laporanLelang');
 
-//admin investasi
-	Route::get('/kelompok-petani','AdminController@kelompokPetani');
-	Route::get('/produk-investasi','AdminController@produkInvestasi');
-	Route::get('/progres-investasi','AdminController@progresInvestasi');
-	Route::get('/pencairan-investasi','AdminController@pencairanInvestasi');
-	Route::get('/laporan-investasi','AdminController@laporanInvestasi');
+// //admin investasi
+// 	Route::get('/kelompok-petani','AdminController@kelompokPetani');
+// 	Route::get('/produk-investasi','AdminController@produkInvestasi');
+// 	Route::get('/progres-investasi','AdminController@progresInvestasi');
+// 	Route::get('/pencairan-investasi','AdminController@pencairanInvestasi');
+// 	Route::get('/laporan-investasi','AdminController@laporanInvestasi');
 
-// });
-// });
+// // });
+// // });
 
 
-//admin login
-Route::get('/masuk_admin','UserController@adminMasuk');
-Route::post('/admin_login','UserController@prosesAdminMasuk');
+// //admin login
+// Route::get('/masuk_admin','UserController@adminMasuk');
+// Route::post('/admin_login','UserController@prosesAdminMasuk');
 
 
 
