b11f06e6be06eb492c41305352954d79c7f6254d
Ahmad Fathoni <38503587+mehmlebu@users.noreply.github.com>
2020-01-24 00:57:10 +0700

    login mitra

--- iCoffee/app/Mitra_tervalidasi.php
+++ iCoffee/app/Mitra_tervalidasi.php
@@ -2,13 +2,10 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 
-class Mitra_tervalidasi extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
+class Mitra_tervalidasi extends Model
 {
-    use Authenticatable, Authorizable, CanResetPassword, Notifiable;
     protected $table = 'mitra_tervalidasi';
     protected $guard = 'mitra';
     protected $fillable = ['nama_koperasi','deskripsi','alamat','jumlah_petani','gambar','id_mitra','email','no_hp','password','kode'];
