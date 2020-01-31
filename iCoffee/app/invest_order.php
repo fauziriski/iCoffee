f64a18a4a2ed76172b63865e1d8fec7e8ba67718
Ahmad Fathoni <38503587+mehmlebu@users.noreply.github.com>
2020-02-01 00:22:34 +0700

    checkout db

--- iCoffee/app/invest_order.php
+++ iCoffee/app/invest_order.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class invest_order extends Model
 {
-    //
+    protected $fillable = ['id_produk','id_investor','id_bank','id_mitra','qty','total','status'];
 }
