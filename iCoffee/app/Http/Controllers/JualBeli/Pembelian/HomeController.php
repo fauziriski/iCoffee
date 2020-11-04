2761832ed3af3a7d35f3a8621620aeff808a879b
Fauzi Riski <38502933+fauziriski@users.noreply.github.com>
2020-11-05 06:56:43 +0700

    push

--- iCoffee/app/Http/Controllers/JualBeli/Pembelian/HomeController.php
+++ iCoffee/app/Http/Controllers/JualBeli/Pembelian/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
 
     public function category($id)
     {
-        $products = Shop_product::where('id_kategori', $id)->orderBy('created_at','desc')->paginate(12);
+        $products = Shop_product::where('id_kategori', $id)->orderBy('created_at','desc')->where('status', 1)->paginate(12);
         $category = Category::all();
 
         if ($products) {
