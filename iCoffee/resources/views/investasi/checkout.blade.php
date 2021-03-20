d89c8ce1aa4c341d63828a07006e0e19507971a8
mehmlebu <ahmad.fathoni50@gmail.com>
2021-03-20 20:03:42 +0700

    snk

--- iCoffee/resources/views/investasi/checkout.blade.php
+++ iCoffee/resources/views/investasi/checkout.blade.php
@@ -2,288 +2,288 @@
 @section('title', 'Biayai Produk Investasi')
 @section('css')
 <style>
-body,html {
-  height:100%;
-  margin:0;
-  font-family:lato;
-}
-.kontainer {
-  -ms-flex-pack:center;
-  justify-content:center;
-  display:-webkit-box;
-  display:-webkit-flex;
-  display:-ms-flexbox;
-  display:flex;
- 
-}
-h2 {
-  margin-bottom:0px;
-  margin-top:25px;
-  font-weight:200;
-  font-size:19px;
-  font-size:1.2rem;
+  body,html {
+    height:100%;
+    margin:0;
+    font-family:lato;
+  }
+  .kontainer {
+    -ms-flex-pack:center;
+    justify-content:center;
+    display:-webkit-box;
+    display:-webkit-flex;
+    display:-ms-flexbox;
+    display:flex;
   
-}
-div.desc {
-  max-width: 500px;
-  min-width: 100px;
-}
-.dropdown-select.visible {
-  display:block;
-}
-.dropdown {
-  position:relative;
-}
-ul {
-  margin:0;
-  padding:0;
-}
-ul li {
-  list-style:none;
-  cursor:pointer;
-}
-ul li:hover {
-  background:rgba(255,255,255,0.1);
-}
-.dropdown-select {
-  position:absolute;
-  background:#1cc98a;
-  text-align:left;
-  box-shadow:0px 3px 5px 0px rgba(0,0,0,0.1);
-  border-bottom-right-radius:5px;
-  border-bottom-left-radius:5px;
-  width:90%;
-  left:2px;
-  line-height:2em;
-  margin-top:2px;
-  box-sizing:border-box;
-}
-.thin {
-  font-weight:400;
-}
-.small {
-  font-size:12px;
-  font-size:.8rem;
-}
-.half-input-table {
-  border-collapse:collapse;
-  width:100%;
-}
-.half-input-table td:first-of-type {
-  border-right:10px solid #1cc98a;
-  width:50%;
-}
-.window {
-  height:540px;
-  width:800px;
-  background:#fff;
-  display:-webkit-box;
-  display:-webkit-flex;
-  display:-ms-flexbox;
-  display:flex;
-  box-shadow: 0px 15px 50px 10px rgba(0, 0, 0, 0.2);
-  border-radius:30px;
-  z-index:10;
-}
-.order-info {
-  height:100%;
-  width:50%;
-  padding-left:25px;
-  padding-right:25px;
-  box-sizing:border-box;
-  display:-webkit-box;
-  display:-webkit-flex;
-  display:-ms-flexbox;
-  display:flex;
-  -webkit-box-pack:center;
-  -webkit-justify-content:center;
-      -ms-flex-pack:center;
-          justify-content:center;
-  position:relative;
-}
-.price {
-  bottom:0px;
-  position:absolute;
-  right:0px;
-  color:#1cc98a;
-}
-.order-table td:first-of-type {
-  width:25%;
-}
-.order-table {
-    position:relative;
-}
-.line {
-  height:1px;
-  width:100%;
-  margin-top:10px;
-  margin-bottom:10px;
-  background:#ddd;
-}
-.order-table td:last-of-type {
-  vertical-align:top;
-  padding-left:25px;
-}
-.order-info-content {
-  table-layout:fixed;
-
-}
-
-.full-width {
-  width:100%;
-}
-.pay-btn {
-  border:none;
-  background:#fff;
-  line-height:2em;
-  border-radius:10px;
-  font-size:19px;
-  font-size:1.2rem;
-  color:#22a877;
-  cursor:pointer;
-  position:absolute;
-  bottom:25px;
-  width:calc(100% - 50px);
-  -webkit-transition:all .2s ease;
-          transition:all .2s ease;
-}
-.pay-btn:hover {
-  background:#00ff8c;
-    color:#eee;
-  -webkit-transition:all .2s ease;
-          transition:all .2s ease;
-}
-
-.total {
-  margin-top:25px;
-  font-size:20px;
-  font-size:1.3rem;
-  position:absolute;
-  bottom:30px;
-  right:27px;
-  left:35px;
-}
-.dense {
-  line-height:1.2em;
-  font-size:16px;
-  font-size:1rem;
-}
-.input-field {
-  background:rgba(255,255,255,0.1);
-  margin-bottom:10px;
-  margin-top:3px;
-  line-height:1.5em;
-  font-size:20px;
-  font-size:1.3rem;
-  border:none;
-  padding:5px 10px 5px 10px;
-  color:#fff;
-  box-sizing:border-box;
-  width:100%;
-  margin-left:auto;
-  margin-right:auto;
-}
-.credit-info {
-  background:#1cc98a;
-  height:100%;
-  width:50%;
-  color:#eee;
-  -webkit-box-pack:center;
-  -webkit-justify-content:center;
-      -ms-flex-pack:center;
-          justify-content:center;
-  font-size:14px;
-  font-size:.9rem;
-  display:-webkit-box;
-  display:-webkit-flex;
-  display:-ms-flexbox;
-  display:flex;
-  box-sizing:border-box;
-  padding-left:25px;
-  padding-right:25px;
-  border-top-right-radius:30px;
-  border-bottom-right-radius:30px;
-  position:relative;
-}
-.dropdown-btn {
-  background:rgba(255,255,255,0.1);
-  width:100%;
-  border-radius:5px;
-  text-align:center;
-  line-height:1.5em;
-  cursor:pointer;
-  position:relative;
-  -webkit-transition:background .2s ease;
-          transition:background .2s ease;
-}
-.dropdown-btn:after {
-  content: '\25BE';
-  right:8px;
-  position:absolute;
-}
-.dropdown-btn:hover {
-  background:rgba(255,255,255,0.2);
-  -webkit-transition:background .2s ease;
-          transition:background .2s ease;
-}
-.dropdown-select {
-  display:none;
-}
-.credit-card-image {
-  display:block;
-  max-height:80px;
-  margin-left:auto;
-  margin-right:auto;
-  margin-top:35px;
-  margin-bottom:15px;
-}
-.credit-info-content {
-  margin-top:25px;
-  -webkit-flex-flow:column;
-      -ms-flex-flow:column;
-          flex-flow:column;
-  display:-webkit-box;
-  display:-webkit-flex;
-  display:-ms-flexbox;
-  display:flex;
-  width:100%;
-}
-@media (max-width: 800px) {
-  .window {
-    width: 100%;
-    height: 100%;
+  }
+  h2 {
+    margin-bottom:0px;
+    margin-top:25px;
+    font-weight:200;
+    font-size:19px;
+    font-size:1.2rem;
+    
+  }
+  div.desc {
+    max-width: 500px;
+    min-width: 100px;
+  }
+  .dropdown-select.visible {
     display:block;
-    border-radius:0px;
+  }
+  .dropdown {
+    position:relative;
+  }
+  ul {
+    margin:0;
+    padding:0;
+  }
+  ul li {
+    list-style:none;
+    cursor:pointer;
+  }
+  ul li:hover {
+    background:rgba(255,255,255,0.1);
+  }
+  .dropdown-select {
+    position:absolute;
+    background:#1cc98a;
+    text-align:left;
+    box-shadow:0px 3px 5px 0px rgba(0,0,0,0.1);
+    border-bottom-right-radius:5px;
+    border-bottom-left-radius:5px;
+    width:90%;
+    left:2px;
+    line-height:2em;
+    margin-top:2px;
+    box-sizing:border-box;
+  }
+  .thin {
+    font-weight:400;
+  }
+  .small {
+    font-size:12px;
+    font-size:.8rem;
+  }
+  .half-input-table {
+    border-collapse:collapse;
+    width:100%;
+  }
+  .half-input-table td:first-of-type {
+    border-right:10px solid #1cc98a;
+    width:50%;
+  }
+  .window {
+    height:540px;
+    width:800px;
+    background:#fff;
+    display:-webkit-box;
+    display:-webkit-flex;
+    display:-ms-flexbox;
+    display:flex;
+    box-shadow: 0px 15px 50px 10px rgba(0, 0, 0, 0.2);
+    border-radius:30px;
+    z-index:10;
   }
   .order-info {
-    width:100%;
-    height:auto;
-    padding-bottom:100px;
-    border-radius:0px;
+    height:100%;
+    width:50%;
+    padding-left:25px;
+    padding-right:25px;
+    box-sizing:border-box;
+    display:-webkit-box;
+    display:-webkit-flex;
+    display:-ms-flexbox;
+    display:flex;
+    -webkit-box-pack:center;
+    -webkit-justify-content:center;
+        -ms-flex-pack:center;
+            justify-content:center;
+    position:relative;
   }
-  .credit-info {
+  .price {
+    bottom:0px;
+    position:absolute;
+    right:0px;
+    color:#1cc98a;
+  }
+  .order-table td:first-of-type {
+    width:25%;
+  }
+  .order-table {
+      position:relative;
+  }
+  .line {
+    height:1px;
     width:100%;
-    height:auto;
-    padding-bottom:100px;
-    border-radius:0px;
+    margin-top:10px;
+    margin-bottom:10px;
+    background:#ddd;
+  }
+  .order-table td:last-of-type {
+    vertical-align:top;
+    padding-left:25px;
+  }
+  .order-info-content {
+    table-layout:fixed;
+
+  }
+
+  .full-width {
+    width:100%;
   }
   .pay-btn {
+    border:none;
+    background:#fff;
+    line-height:2em;
     border-radius:10px;
+    font-size:19px;
+    font-size:1.2rem;
+    color:#22a877;
+    cursor:pointer;
+    /* position:absolute; */
+    bottom:25px;
+    width:calc(100% - 50px);
+    -webkit-transition:all .2s ease;
+            transition:all .2s ease;
   }
-}
-.chartist-tooltip {
-  opacity: 0;
-  position: absolute;
-  margin: 20px 0 0 10px;
-  background: rgba(0, 0, 0, 0.8);
-  color: #FFF;
-  padding: 5px 10px;
-  border-radius: 4px;
-}
+  .pay-btn:hover {
+    background:#00ff8c;
+      color:#eee;
+    -webkit-transition:all .2s ease;
+            transition:all .2s ease;
+  }
 
-.chartist-tooltip.tooltip-show {
-  opacity: 1;
-}
+  .total {
+    margin-top:25px;
+    font-size:20px;
+    font-size:1.3rem;
+    position:absolute;
+    bottom:30px;
+    right:27px;
+    left:35px;
+  }
+  .dense {
+    line-height:1.2em;
+    font-size:16px;
+    font-size:1rem;
+  }
+  .input-field {
+    background:rgba(255,255,255,0.1);
+    margin-bottom:10px;
+    margin-top:3px;
+    line-height:1.5em;
+    font-size:20px;
+    font-size:1.3rem;
+    border:none;
+    padding:5px 10px 5px 10px;
+    color:#fff;
+    box-sizing:border-box;
+    width:100%;
+    margin-left:auto;
+    margin-right:auto;
+  }
+  .credit-info {
+    background:#1cc98a;
+    height:100%;
+    width:50%;
+    color:#eee;
+    -webkit-box-pack:center;
+    -webkit-justify-content:center;
+        -ms-flex-pack:center;
+            justify-content:center;
+    font-size:14px;
+    font-size:.9rem;
+    display:-webkit-box;
+    display:-webkit-flex;
+    display:-ms-flexbox;
+    display:flex;
+    box-sizing:border-box;
+    padding-left:25px;
+    padding-right:25px;
+    border-top-right-radius:30px;
+    border-bottom-right-radius:30px;
+    position:relative;
+  }
+  .dropdown-btn {
+    background:rgba(255,255,255,0.1);
+    width:100%;
+    border-radius:5px;
+    text-align:center;
+    line-height:1.5em;
+    cursor:pointer;
+    position:relative;
+    -webkit-transition:background .2s ease;
+            transition:background .2s ease;
+  }
+  .dropdown-btn:after {
+    content: '\25BE';
+    right:8px;
+    position:absolute;
+  }
+  .dropdown-btn:hover {
+    background:rgba(255,255,255,0.2);
+    -webkit-transition:background .2s ease;
+            transition:background .2s ease;
+  }
+  .dropdown-select {
+    display:none;
+  }
+  .credit-card-image {
+    display:block;
+    max-height:80px;
+    margin-left:auto;
+    margin-right:auto;
+    margin-top:35px;
+    margin-bottom:15px;
+  }
+  .credit-info-content {
+    margin-top:25px;
+    -webkit-flex-flow:column;
+        -ms-flex-flow:column;
+            flex-flow:column;
+    display:-webkit-box;
+    display:-webkit-flex;
+    display:-ms-flexbox;
+    display:flex;
+    width:100%;
+  }
+  @media (max-width: 800px) {
+    .window {
+      width: 100%;
+      height: 100%;
+      display:block;
+      border-radius:0px;
+    }
+    .order-info {
+      width:100%;
+      height:auto;
+      padding-bottom:100px;
+      border-radius:0px;
+    }
+    .credit-info {
+      width:100%;
+      height:auto;
+      padding-bottom:100px;
+      border-radius:0px;
+    }
+    .pay-btn {
+      border-radius:10px;
+    }
+  }
+  .chartist-tooltip {
+    opacity: 0;
+    position: absolute;
+    margin: 20px 0 0 10px;
+    background: rgba(0, 0, 0, 0.8);
+    color: #FFF;
+    padding: 5px 10px;
+    border-radius: 4px;
+  }
+
+  .chartist-tooltip.tooltip-show {
+    opacity: 1;
+  }
 </style>	
 @endsection
 @section('sidebar')
@@ -318,27 +318,7 @@ ul li:hover {
 					</table>
           <div class='line'></div>
           <br>
-          <div class="chart-area">
-            <canvas id="myAreaChart"></canvas>
-          </div>
-				  {{-- <table class='order-table'>
-					<tbody>
-						<h2>Mitra Proyek</h2>
-					<tr>
-						<td><img src="{{ asset('Uploads/Investasi/Logo.jpg')}}" style="border-radius: 40%" class='full-width'></img>
-						</td>
-						<td>
-						 <span class='thin'>{{$mitra->nama}}</span>
-						<br> <span class='thin small'> Email: {{$mitra->email}} <br>Petani: {{$mitra->jumlah_petani}} Orang<br></span>
-					</td>
-		
-					</tr>
-						
-				
-					</tbody>
-					
-				</table>
-				<div class="desc" align="justify">{{ Str::limit('Untuk program kemitraan pembenihan jagung kali ini akan dilakukan oleh Kelompok Tani Eko Proyo yang berada di Desa Sukoanyar, Kecamatan Wajak, Kabupaten Malang, Provinsi Jawa Timur yang akan memberdayakan ± 120 orang lebih petani jagung dengan rencana luas garapan ± 40 hektar serta estimasi produksi benih jagung hibrida sebesar ± 2 ton per hektar dalam 1 (satu) siklus produksi.',285) }}</div> --}}
+          
 			  
 	  </div>
 	  </div>
@@ -355,7 +335,7 @@ ul li:hover {
 						</div>
 						</td></tr>
 					</table>
-					<img src='\bni.png' height='80' class='credit-card-image' id='credit-card-image'></img>
+					<img src='\bni.png' height='80' class='credit-card-image' id='credit-card-image'>
 					<div id="norek">Nomor Rekening : 187823287 a.n. iCoffee</div>
 					<form action="/invest/checkout/berhasil" method="POST">
 						@csrf
@@ -364,7 +344,14 @@ ul li:hover {
 						<input type="hidden" name="id_produk" value="{{$produk->id}}">
             <input type="hidden" name="id_mitra" value="{{$produk->id_mitra}}">
             <input type="hidden" name="id_bank" id="bank" value="3">
-						<button class='pay-btn'>Mulai Membiayai Petani</button>
+            <div class="text-justify mt-5">
+              <input class="form-check-input" type="checkbox" name="snk" required>
+              <label class="form-check-label" for="defaultCheck1">
+                  Dengan ini saya mendaftar dan menyetujui <a href="#exampleModal" data-toggle="modal" data-target="#exampleModal"> Syarat dan Ketentuan</a> pengguna.
+              </label>
+              <button class='pay-btn'>Mulai Membiayai Petani</button>
+            </div>
+            
 					</form>
 				  
 	  
@@ -373,7 +360,29 @@ ul li:hover {
 			  </div>
 			</div>
 	  </div>
-
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Syarat dan Ketentuan</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+              </div>
+              <div class="modal-body">
+                  <ul>
+                    <li><p class="text-justify">⚫ Pembagian Hasil Investasi adalah: 40% Petani, 40% Investor, 20% iCoffee.</p></li>
+                    <li><p class="text-justify">⚫ Jika terjadi gagal panen, maka iCoffee tidak memiliki kewajiban untuk mengembalikan dana investor.</p></li>
+                    <li><p class="text-justify">⚫ Investor berhak mendapatkan pertanggungjawaban dalam bentuk progress untuk setiap produk investasi yang dibiayai.</p></li>
+                    <li><p class="text-justify">⚫ Dengan menekan tombol checklist, investor dianggap menyetujui seluruh syarat & ketentuan yang berlaku.</p></li>
+                  </ul>
+              </div>
+              <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+          </div>
+      </div>
+  </div>
     @php
       $loop = $produk->periode/$produk->profit_periode;
       $profit = $produk->harga*($produk->roi/100)*$qty;
@@ -421,9 +430,7 @@ window.onclick = function(e) {
       "3. Masukkan PIN ATM Anda. <br/>" +
       "4. Pilih 'Menu Lainnya'. <br/>" +
       "5. Pilih 'Transfer'. <br/>" +
-      "6. Pilih Jenis rekening yang akan Anda gunakan (Contoh: 'Dari Rekening Tabungan'). <br/>" +
-      "7. Pilih 'Virtual Account Billing'. <br/>" +
-      "8. Masukkan nomor Virtual Account Anda (contoh: 8277895360122288). <br/>";
+      "6. Pilih Jenis rekening yang akan Anda gunakan (Contoh: 'Dari Rekening Tabungan'). <br/>";
       }
     else if (e.target.innerHTML === 'Bank Mandiri') {
          document.getElementById('credit-card-image').src = '/mandiri.svg';
